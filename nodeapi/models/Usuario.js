ad43a0be1b2ef793b982a08de6c3a7019ee7b9f2
jamg44 <jamg44@gmail.com>
2018-10-19 00:35:15 +0200

    enviar emails: metemos nodemailer

--- nodeapi/models/Usuario.js
+++ nodeapi/models/Usuario.js
@@ -2,6 +2,23 @@
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
+
+// configuramos transport de nodemailer
+const transport = nodemailer.createTransport({
+  service: 'SendGrid',
+  auth: {
+    user: 'javierkeepcoding',
+    pass: 'Test1234'
+  }
+});
+
+// transport.sendMail({
+//   to: 'jamg44@gmail.com',
+//   from: 'NodeAPI <admin@nodeapi.com>',
+//   subject: 'Compra confirmada',
+//   text: 'Tu compra ha sido verificada'
+// });
 
 const usuarioSchema = mongoose.Schema({
   name: String,
@@ -11,6 +28,10 @@ const usuarioSchema = mongoose.Schema({
 
 usuarioSchema.statics.hashPassword = function(plainPassword) {
   return bcrypt.hash(plainPassword, 10);
+}
+
+usuarioSchema.methods.sendEmail = function() {
+
 }
 
 const Usuario = mongoose.model('Usuario', usuarioSchema);
