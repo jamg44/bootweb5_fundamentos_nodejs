1c168afb71ec153bfbeb9eb7bc4bdc23ebff0992
jamg44 <jamg44@gmail.com>
2018-10-23 22:20:00 +0200

    websockets: parte 2, añadiendo websockets

--- ejemplos/ejemplo_websockets/server.js
+++ ejemplos/ejemplo_websockets/server.js
@@ -3,8 +3,18 @@
 const app = require('express')();
 const server = require('http').Server(app);
 
+const io = require('socket.io')(server);
+
 app.get('/', (req, res, next) => {
-  res.send('ok');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('nueva conexion de un cliente', socket.id);
+  socket.on('chat message', msg => {
+    console.log('mensaje recibido', msg);
+    io.emit('chat message', msg);
+  })
 });
 
 server.listen(3000, ()=> {
